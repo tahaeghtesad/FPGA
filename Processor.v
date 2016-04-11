6c8b5dba62c6cb07c75cd06df5ce17f2dca8a0a8
Zohre Rezayat <zo.rezayat@gmail.com>
2016-04-11 12:24:23 +0430

    more changes to processor

--- Processor.v
+++ Processor.v
@@ -22,20 +22,52 @@ module Processor(clk, dip, key, led, sevenseg
     );
 	 input clk;
 	 input [15:0]dip;
-	 input [4:0] key;
+	 input [4:0]key;
 	 output [15:0]led;
 	 output [12:0]sevenseg;
 	 
-	 reg [7:0] eom;
+	 reg [7:0] eom, pc, ir;
+	 wire [7:0] imaddr;
 	 wire we;
 	 wire[15:0] data;
 	 
+	 /*
+	 assign imaddr = key[2] ? eom : ir;
+	 assign we = key[0] ? 1 : 0;
+	 
 	 InstructionMemory im(
-		.addra(eom),
+		.addra(imaddr),
 		.dina(dip),
 		.wea(we),
 		.clka(clk),
 		.douta(data)
 		);
+	*/
+	
+	assign sevenseg = {5'd0,eom};
+	assign led = {8'd0,ir};
+	
+	/*
+	always @(posedge key[0], posedge key[2])
+	begin
+		if(key[2])
+			eom = 0;
+		else
+			eom = eom + 1;
+				
+	end
+	always @(posedge key[1], posedge key[3])
+	begin
+		if(key[3])
+			pc =0;
+		else
+			begin
+				pc = pc + 1;
+				ir = data;
+			end		
+	end */
+	
+	always @(posedge key[0])
+		eom = eom + 1;
 	
 endmodule
